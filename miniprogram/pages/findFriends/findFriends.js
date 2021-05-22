73c74cf60f5915de72ad1b142bb180e32c37415d
LiChao <1172274095@qq.com>
2021-05-22 16:44:08 +0800

    更新提示信息

--- miniprogram/pages/findFriends/findFriends.js
+++ miniprogram/pages/findFriends/findFriends.js
@@ -73,7 +73,9 @@ Page({
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
-
+        // wx.setNavigationBarTitle({
+        //     title: '查找地图上显示的朋友'
+        //   })
     },
 
     /**
